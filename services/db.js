329a3d5afe92dcd1447f4e0211b03632d5c2d266
prasanthc <prasanthgeek@gmail.com>
2014-11-20 16:13:26 +0530

    database configuration changed

--- services/db.js
+++ services/db.js
@@ -1,7 +1,7 @@
 var Db = require('mysql-activerecord');
 
 var pool = new Db.Pool({
-    server: '127.0.0.1',
+    server: 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/',
     username: 'adminmVpqdHM',
     password: 'E7MhcAeUBeWl',
     database: 'secretypost'
